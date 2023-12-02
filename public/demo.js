5474ab66358cb0e95aa82d960594507574a976df
phantrunghieu91 <phantrunghieu1291@gmail.com>
2023-12-03 00:20:25 +0700

    render list method

--- public/demo.js
+++ public/demo.js
@@ -1,2 +1,50 @@
 "use strict";
-console.log('Hello world!');
+class TodoApp {
+    constructor() {
+        this.count = 0;
+        this.todoList = [{ id: 1, text: 'test' }];
+        this.root = document.querySelector('#root');
+        this.form = document.querySelector('#todo-form');
+        this.listDiv = document.querySelector('#todo-list');
+        this.renderList();
+    }
+    renderList() {
+        var _a, _b;
+        const title = (_a = this.listDiv) === null || _a === void 0 ? void 0 : _a.querySelector('h2');
+        const listOl = (_b = this.listDiv) === null || _b === void 0 ? void 0 : _b.querySelector('ol');
+        if (this.todoList.length === 0 && this.listDiv) {
+            if (title)
+                title.textContent = 'Nothing here';
+            if (listOl)
+                listOl.innerHTML = '';
+        }
+        else if (this.todoList.length !== 0 && this.listDiv) {
+            if (title)
+                title.textContent = 'todo list:';
+            this.todoList.forEach(todo => {
+                var _a;
+                (_a = document.querySelector('#todo-list > ol')) === null || _a === void 0 ? void 0 : _a.append(this.renderTodoItem(todo));
+            });
+        }
+    }
+    renderTodoItem(todo) {
+        const todoDiv = document.createElement('li');
+        todoDiv.className = 'todo-item';
+        const text = document.createElement('p');
+        text.textContent = todo.text;
+        const editBtn = document.createElement('button');
+        const removeBtn = document.createElement('button');
+        editBtn.type = removeBtn.type = 'button';
+        editBtn.textContent = 'edit';
+        removeBtn.textContent = 'remove';
+        removeBtn.addEventListener('click', event => {
+            this.todoList = this.todoList.filter(el => el.id !== todo.id);
+            this.renderList();
+        });
+        todoDiv.append(text, editBtn, removeBtn);
+        return todoDiv;
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    const app = new TodoApp();
+});
--- src/demo.ts
+++ src/demo.ts
@@ -1,1 +1,57 @@
-console.log('Hello world!');
+interface TodoInterface {
+  id: number;
+  text: string;
+  isCompleted?: boolean;
+}
+
+class TodoApp {
+  private count = 0;
+  private todoList: TodoInterface[] = [{ id: 1, text: 'test' }];
+  private updatingTodo: undefined | TodoInterface;
+  private root = document.querySelector('#root');
+  private form = document.querySelector('#todo-form');
+  private listDiv = document.querySelector('#todo-list');
+
+  constructor() {
+    this.renderList();
+  }
+
+  private renderList() {
+    const title = this.listDiv?.querySelector('h2');
+    const listOl = this.listDiv?.querySelector('ol');
+    if (this.todoList.length === 0 && this.listDiv) {
+      if (title) title.textContent = 'Nothing here';
+      if (listOl) listOl.innerHTML = '';
+    } else if (this.todoList.length !== 0 && this.listDiv) {
+      if (title) title.textContent = 'todo list:';
+      this.todoList.forEach(todo => {
+        document.querySelector('#todo-list > ol')?.append(this.renderTodoItem(todo));
+      });
+    }
+  }
+
+  private renderTodoItem(todo: TodoInterface): HTMLElement {
+    const todoDiv = document.createElement('li');
+    todoDiv.className = 'todo-item';
+    const text = document.createElement('p');
+    text.textContent = todo.text;
+    const editBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+    editBtn.type = removeBtn.type = 'button';
+    editBtn.textContent = 'edit';
+    removeBtn.textContent = 'remove';
+
+    removeBtn.addEventListener('click', event => {
+      this.todoList = this.todoList.filter(el => el.id !== todo.id);
+      this.renderList();
+    });
+
+    todoDiv.append(text, editBtn, removeBtn);
+
+    return todoDiv;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const app = new TodoApp();
+});
