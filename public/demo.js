ea867d4ae0ff3e933fb43d9955b07155dc2d4164
phantrunghieu91 <phantrunghieu1291@gmail.com>
2023-12-04 08:51:52 +0700

    basic todo app

--- public/demo.js
+++ public/demo.js
@@ -1,17 +1,75 @@
 "use strict";
 class TodoApp {
     constructor() {
-        this.count = 0;
+        var _a, _b, _c, _d;
+        this.count = 1;
         this.todoList = [{ id: 1, text: 'test' }];
-        this.root = document.querySelector('#root');
         this.form = document.querySelector('#todo-form');
+        this.modal = document.querySelector('#modal');
         this.listDiv = document.querySelector('#todo-list');
         this.renderList();
+        (_a = this.form) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', event => {
+            var _a;
+            event.preventDefault();
+            const input = (_a = this.form) === null || _a === void 0 ? void 0 : _a.querySelector('input');
+            if (!this.updatingTodo && input) {
+                this.todoList.push({ id: ++this.count, text: input.value });
+                input.value = '';
+            }
+            if (this.updatingTodo && input) {
+                this.updatingTodo.text = input.value;
+                this.todoList = this.todoList.map(todo => {
+                    var _a;
+                    if (this.updatingTodo !== undefined && todo.id === ((_a = this.updatingTodo) === null || _a === void 0 ? void 0 : _a.id))
+                        todo = this.updatingTodo;
+                    return todo;
+                });
+                this.setUpdatingTodo(undefined);
+            }
+            this.renderList();
+        });
+        (_b = this.form) === null || _b === void 0 ? void 0 : _b.addEventListener('reset', event => {
+            var _a;
+            (_a = this.form) === null || _a === void 0 ? void 0 : _a.reset();
+            this.setUpdatingTodo(undefined);
+        });
+        (_d = (_c = this.modal) === null || _c === void 0 ? void 0 : _c.querySelector('.close-btn')) === null || _d === void 0 ? void 0 : _d.addEventListener('click', event => {
+            var _a;
+            (_a = this.modal) === null || _a === void 0 ? void 0 : _a.close();
+        });
+    }
+    setUpdatingTodo(todo) {
+        this.updatingTodo = todo;
+        this.changeFormForUpdate();
+    }
+    changeFormForUpdate() {
+        if (this.form) {
+            const title = this.form.querySelector('h2');
+            const input = this.form.querySelector('input');
+            const submitBtn = this.form.querySelector('button[type=submit]');
+            const updatingTodo = this.updatingTodo;
+            if (title && updatingTodo && input && submitBtn) {
+                title.textContent = `edit "${updatingTodo.text}":`;
+                if (updatingTodo === undefined)
+                    input.value = '';
+                if (updatingTodo !== undefined)
+                    input.value = updatingTodo.text;
+                submitBtn.textContent = 'edit';
+            }
+            if (title && !this.updatingTodo && input && submitBtn) {
+                title.textContent = `add new todo:`;
+                input.value = '';
+                submitBtn.textContent = 'add';
+            }
+        }
     }
     renderList() {
         var _a, _b;
+        // console.log(this.todoList);
         const title = (_a = this.listDiv) === null || _a === void 0 ? void 0 : _a.querySelector('h2');
         const listOl = (_b = this.listDiv) === null || _b === void 0 ? void 0 : _b.querySelector('ol');
+        if (listOl)
+            listOl.innerHTML = '';
         if (this.todoList.length === 0 && this.listDiv) {
             if (title)
                 title.textContent = 'Nothing here';
@@ -37,12 +95,61 @@ class TodoApp {
         editBtn.type = removeBtn.type = 'button';
         editBtn.textContent = 'edit';
         removeBtn.textContent = 'remove';
+        text.addEventListener('click', event => {
+            const _self = event.currentTarget;
+            if (_self) {
+                if (todo.isCompleted) {
+                    _self.classList.remove('completed');
+                    todo.isCompleted = false;
+                }
+                else {
+                    _self.classList.add('completed');
+                    todo.isCompleted = true;
+                }
+            }
+        });
+        editBtn.addEventListener('click', event => {
+            this.setUpdatingTodo(todo);
+        });
         removeBtn.addEventListener('click', event => {
-            this.todoList = this.todoList.filter(el => el.id !== todo.id);
-            this.renderList();
+            var _a;
+            this.renderVerifyRemoveModal(todo);
+            (_a = this.modal) === null || _a === void 0 ? void 0 : _a.showModal();
         });
         todoDiv.append(text, editBtn, removeBtn);
         return todoDiv;
+    }
+    renderVerifyRemoveModal(todo) {
+        var _a, _b, _c;
+        const title = (_a = this.modal) === null || _a === void 0 ? void 0 : _a.querySelector('.title');
+        if (title)
+            title.textContent = `Remove "${todo.text}"`;
+        const main = (_b = this.modal) === null || _b === void 0 ? void 0 : _b.querySelector('main');
+        if (main) {
+            main.innerHTML = '';
+            const message = document.createElement('p');
+            message.textContent = `Are you sure want to remove "${todo.text}" ?`;
+            main.append(message);
+        }
+        const footer = (_c = this.modal) === null || _c === void 0 ? void 0 : _c.querySelector('footer');
+        if (footer) {
+            footer.innerHTML = '';
+            const submitBtn = document.createElement('button');
+            const cancelBtn = document.createElement('button');
+            submitBtn.textContent = 'Accept';
+            cancelBtn.textContent = 'Cancel';
+            submitBtn.addEventListener('click', () => {
+                var _a;
+                this.todoList = this.todoList.filter(el => el.id !== todo.id);
+                this.renderList();
+                (_a = this.modal) === null || _a === void 0 ? void 0 : _a.close();
+            });
+            cancelBtn.addEventListener('click', () => {
+                var _a;
+                (_a = this.modal) === null || _a === void 0 ? void 0 : _a.close();
+            });
+            footer.append(cancelBtn, submitBtn);
+        }
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
--- src/demo.ts
+++ src/demo.ts
@@ -1,24 +1,76 @@
-interface TodoInterface {
+type TodoType = {
   id: number;
   text: string;
   isCompleted?: boolean;
-}
+};
 
 class TodoApp {
-  private count = 0;
-  private todoList: TodoInterface[] = [{ id: 1, text: 'test' }];
-  private updatingTodo: undefined | TodoInterface;
-  private root = document.querySelector('#root');
-  private form = document.querySelector('#todo-form');
+  private count = 1;
+  private todoList: TodoType[] = [{ id: 1, text: 'test' }];
+  private updatingTodo: undefined | TodoType;
+  private form: HTMLFormElement | null = document.querySelector('#todo-form');
+  private modal: HTMLDialogElement | null = document.querySelector('#modal');
   private listDiv = document.querySelector('#todo-list');
 
   constructor() {
     this.renderList();
+    this.form?.addEventListener('submit', event => {
+      event.preventDefault();
+      const input = this.form?.querySelector('input');
+
+      if (!this.updatingTodo && input) {
+        this.todoList.push({ id: ++this.count, text: input.value });
+        input.value = '';
+      }
+      if (this.updatingTodo && input) {
+        this.updatingTodo.text = input.value;
+        this.todoList = this.todoList.map(todo => {
+          if (this.updatingTodo !== undefined && todo.id === this.updatingTodo?.id) todo = this.updatingTodo;
+          return todo;
+        });
+        this.setUpdatingTodo(undefined);
+      }
+      this.renderList();
+    });
+    this.form?.addEventListener('reset', event => {
+      this.form?.reset();
+      this.setUpdatingTodo(undefined);
+    });
+    this.modal?.querySelector('.close-btn')?.addEventListener('click', event => {
+      this.modal?.close();
+    });
+  }
+
+  setUpdatingTodo(todo: undefined | TodoType) {
+    this.updatingTodo = todo;
+    this.changeFormForUpdate();
+  }
+
+  changeFormForUpdate() {
+    if (this.form) {
+      const title = this.form.querySelector('h2');
+      const input = this.form.querySelector('input');
+      const submitBtn = this.form.querySelector('button[type=submit]');
+      const updatingTodo = this.updatingTodo;
+      if (title && updatingTodo && input && submitBtn) {
+        title.textContent = `edit "${updatingTodo.text}":`;
+        if (updatingTodo === undefined) input.value = '';
+        if (updatingTodo !== undefined) input.value = updatingTodo.text;
+        submitBtn.textContent = 'edit';
+      }
+      if (title && !this.updatingTodo && input && submitBtn) {
+        title.textContent = `add new todo:`;
+        input.value = '';
+        submitBtn.textContent = 'add';
+      }
+    }
   }
 
   private renderList() {
+    // console.log(this.todoList);
     const title = this.listDiv?.querySelector('h2');
     const listOl = this.listDiv?.querySelector('ol');
+    if (listOl) listOl.innerHTML = '';
     if (this.todoList.length === 0 && this.listDiv) {
       if (title) title.textContent = 'Nothing here';
       if (listOl) listOl.innerHTML = '';
@@ -30,7 +82,7 @@ class TodoApp {
     }
   }
 
-  private renderTodoItem(todo: TodoInterface): HTMLElement {
+  private renderTodoItem(todo: TodoType): HTMLElement {
     const todoDiv = document.createElement('li');
     todoDiv.className = 'todo-item';
     const text = document.createElement('p');
@@ -41,14 +93,64 @@ class TodoApp {
     editBtn.textContent = 'edit';
     removeBtn.textContent = 'remove';
 
+    text.addEventListener('click', event => {
+      const _self = event.currentTarget as HTMLElement;
+      if (_self) {
+        if (todo.isCompleted) {
+          _self.classList.remove('completed');
+          todo.isCompleted = false;
+        } else {
+          _self.classList.add('completed');
+          todo.isCompleted = true;
+        }
+      }
+    });
+
+    editBtn.addEventListener('click', event => {
+      this.setUpdatingTodo(todo);
+    });
+
     removeBtn.addEventListener('click', event => {
-      this.todoList = this.todoList.filter(el => el.id !== todo.id);
-      this.renderList();
+      this.renderVerifyRemoveModal(todo);
+      this.modal?.showModal();
     });
 
     todoDiv.append(text, editBtn, removeBtn);
 
     return todoDiv;
+  }
+
+  private renderVerifyRemoveModal(todo: TodoType) {
+    const title = this.modal?.querySelector('.title');
+    if (title) title.textContent = `Remove "${todo.text}"`;
+
+    const main = this.modal?.querySelector('main');
+    if (main) {
+      main.innerHTML = '';
+      const message = document.createElement('p');
+      message.textContent = `Are you sure want to remove "${todo.text}" ?`;
+      main.append(message);
+    }
+
+    const footer = this.modal?.querySelector('footer');
+    if (footer) {
+      footer.innerHTML = '';
+      const submitBtn = document.createElement('button');
+      const cancelBtn = document.createElement('button');
+      submitBtn.textContent = 'Accept';
+      cancelBtn.textContent = 'Cancel';
+
+      submitBtn.addEventListener('click', () => {
+        this.todoList = this.todoList.filter(el => el.id !== todo.id);
+        this.renderList();
+        this.modal?.close();
+      });
+
+      cancelBtn.addEventListener('click', () => {
+        this.modal?.close();
+      });
+      footer.append(cancelBtn, submitBtn);
+    }
   }
 }
 
